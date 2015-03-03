aa296d1124749e1bb33e3c0039936518445f2126
cbmland <cbmland@qq.com>
2015-03-04 01:11:08 +0800

    login2

--- cloud/app.js
+++ cloud/app.js
@@ -7,6 +7,12 @@ app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
+// 启用 cookieParser
+app.use(express.cookieParser('Your Cookie Secure'));
+// 使用 avos-express-cookie-session 记录登录信息到 cookie
+app.use(avosExpressCookieSession({ cookie: { maxAge: 3600000 }, fetchUser: true}));
+
+
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
