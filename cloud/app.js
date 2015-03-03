9eb00460c7f2891a816865e2f594f3e68efe0d44
cbmland <cbmland@qq.com>
2015-03-04 01:16:27 +0800

    cookies

--- cloud/app.js
+++ cloud/app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
 // 启用 cookieParser
-app.use(express.cookieParser('Your Cookie Secure'));
+app.use(express.cookieParser('LKJHYUiop58746ASDCBM'));
 // 使用 avos-express-cookie-session 记录登录信息到 cookie
 app.use(avosExpressCookieSession({ cookie: { maxAge: 3600000 }, fetchUser: true}));
 
