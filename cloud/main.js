d0709f9f6720e0f091e0a7db9368e9858805406e
cbmland <cbmland@qq.com>
2015-03-04 00:04:04 +0800

    hello

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,10 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+	var name = request.params.name;
+	if (name) {
+		response.success('Hello ' + name +'!');
+	} else {
+		response.error('what your name?')
+	}
 });
