1a4c27b7e58370715a8721b0dd9cbbdf16c1bb7b
cbmland <cbmland@qq.com>
2015-03-04 00:30:51 +0800

    web hosting

--- cloud/main.js
+++ cloud/main.js
@@ -1,10 +1,6 @@
+require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-	var name = request.params.name;
-	if (name) {
-		response.success('Hello ' + name +'!');
-	} else {
-		response.error('what your name?')
-	}
+  response.success("Hello world!");
 });
