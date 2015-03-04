0d7c4925db25fd43cee296e8f4a15318269f6c23
allenfeng <allenfeng@tencent.com>
2015-03-04 17:04:44 +0800

    weixin

--- cloud/app.js
+++ cloud/app.js
@@ -1,98 +1,75 @@
-// 在 Cloud code 里初始化 Express 框架
 var express = require('express');
+var xml2js = require('xml2js');
+var weixin = require('cloud/weixin.js');
+var utils = require('express/node_modules/connect/lib/utils');
+
+// 解析微信的 xml 数据
+var xmlBodyParser = function (req, res, next) {
+  if (req._body) return next();
+  req.body = req.body || {};
+
+  // ignore GET
+  if ('GET' == req.method || 'HEAD' == req.method) return next();
+
+  // check Content-Type
+  if ('text/xml' != utils.mime(req)) return next();
+
+  // flag as parsed
+  req._body = true;
+
+  // parse
+  var buf = '';
+  req.setEncoding('utf8');
+  req.on('data', function(chunk){ buf += chunk });
+  req.on('end', function(){  
+    xml2js.parseString(buf, function(err, json) {
+      if (err) {
+          err.status = 400;
+          next(err);
+      } else {
+          req.body = json;
+          next();
+      }
+    });
+  });
+};
+
 var app = express();
-var avosExpressCookieSession = require('avos-express-cookie-session');
 
 // App 全局配置
-
-if(__production)
-  app.set('views', 'cloud/views');
-else
-  app.set('views', 'cloud/dev_views');
-
-//app.set('views','cloud/views');   // 设置模板目录
+app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
-
-// 启用 cookieParser
-app.use(express.cookieParser('LKJHYUiop58746ASDCBM'));
-// 使用 avos-express-cookie-session 记录登录信息到 cookie
-app.use(avosExpressCookieSession({ cookie: { maxAge: 3600000 }, fetchUser: true}));
-
-var avosExpressHttpsRedirect = require('avos-express-https-redirect');
-app.use(avosExpressHttpsRedirect());
-
+app.use(xmlBodyParser);
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
-app.get('/g', function(req, res) {
-	AV.Cloud.httpRequest({
-	  url: 'http://www.baidu.com/',
-	  success: function(httpResponse) {
-		res.send(httpResponse.text);
-	  },
-	  error: function(httpResponse) {
-		console.error('Request failed with response code ' + httpResponse.status);
-	  }
-	});
-});
-
-var fs = require('fs');
-app.post('/upload', function(req, res){
-  var iconFile = req.files.iconImage;
-  if(iconFile){
-    fs.readFile(iconFile.path, function(err, data){
-      if(err)
-        return res.send('读取文件失败');
-      var base64Data = data.toString('base64');
-      var theFile = new AV.File(iconFile.name, {base64: base64Data});
-      theFile.save().then(function(theFile){
-        res.send('上传成功！');
-      });
-    });
-  }else
-    res.send('请选择一个文件。');
-});
-
-
-app.get('/login', function(req, res) {
-    // 渲染登录页面
-    res.render('login.ejs');
-});
-// 点击登录页面的提交将出发下列函数
-app.post('/login', function(req, res) {
-    AV.User.logIn(req.body.username, req.body.password).then(function() {
-      //登录成功，avosExpressCookieSession会自动将登录用户信息存储到cookie
-      //跳转到profile页面。
-      console.log('signin successfully: %j', req.AV.user);
-      res.redirect('/profile');
-    },function(error) {
-      //登录失败，跳转到登录页面
-      res.redirect('/login');
-  });
-});
-//查看用户profile信息
-app.get('/profile', function(req, res) {
-    // 判断用户是否已经登录
-    if (req.AV.user) {
-      // 如果已经登录，发送当前登录用户信息。
-      res.send(req.AV.user);
-    } else {
-      // 没有登录，跳转到登录页面。
-      res.redirect('/login');
+app.get('/weixin', function(req, res) {
+  console.log('weixin req:', req.query);
+  weixin.exec(req.query, function(err, data) {
+    if (err) {
+      return res.send(err.code || 500, err.message);
     }
-});
+    return res.send(data);
+  });
+})
 
-//调用此url来登出帐号
-app.get('/logout', function(req, res) {
-  //avosExpressCookieSession将自动清除登录cookie信息
-    AV.User.logOut();
-    res.redirect('/profile');
-});
-
+app.post('/weixin', function(req, res) {
+  console.log('weixin req:', req.body);
+  weixin.exec(req.body, function(err, data) {
+    if (err) {
+      return res.send(err.code || 500, err.message);
+    }
+    var builder = new xml2js.Builder();
+    var xml = builder.buildObject(data);
+    console.log('res:', data)
+    res.set('Content-Type', 'text/xml');
+    return res.send(xml);
+  });
+})
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
