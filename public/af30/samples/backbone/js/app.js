ec7b56e60b4022bd94cf9e26f5f714bf2d97690f
allenfeng <allenfeng@tencent.com>
2015-04-16 16:54:59 +0800

    imageflip2

--- public/af30/samples/backbone/js/app.js
+++ public/af30/samples/backbone/js/app.js
@@ -8,17 +8,26 @@
         el: '#main',
         events: {
             'click #addTodo': 'addTodo',
+            'click .thumb': 'showPhoto',
             'longTap li': 'removeTodo'
         },
         template:_.template($('#todoTemplate').html()),
         addTodo:function(){
 
             var val='元素item';
-            var imgUrl = 'http://ac-0rg4booz.clouddn.com/53cfcd2314328ec8.jpg?imageView/2/w/200/h/200/q/60/format/jpg'
+            var imgUrl = 'http://ac-0rg4booz.clouddn.com/53cfcd2314328ec8.jpg?imageView/2/w/200/h/200/q/60/format/jpg';
 
             todos.push(val);
 
             $("#todoList").append(this.template({title:val,img:imgUrl}));
+        },
+        showPhoto:function(e)
+        {
+            //$('#imgBox').css( "background:url('http://ac-0rg4booz.clouddn.com/53cfcd2314328ec8.jpg') center no-repeat" );
+            $('#imgSmall').attr('src',"http://ac-0rg4booz.clouddn.com/53cfcd2314328ec8.jpg?imageView/2/w/200/h/200/q/60/format/jpg");
+
+            $('#imgBig').attr('src','http://ac-0rg4booz.clouddn.com/53cfcd2314328ec8.jpg');
+            $.afui.loadContent('#photo',false,false,'pop');
         },
         removeTodo:function(e){
             var item=$(e.target);
